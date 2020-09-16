f8eb5799661a3eb5b72003005518730bb3bf7d68
guillermo <gblasco@bloobirds.com>
2020-09-16 22:36:06 +0200

    1-1-ordinals

--- level-1/1-1-ordinals/index.js
+++ level-1/1-1-ordinals/index.js
@@ -1,7 +1,23 @@
 
 const numberToOrdinal = number => {
-  /* Your code here */
-  return "1st";
+  // all the cases the number ends in 11, 12, 13: it ends in th
+  if (number % 100 > 10 && number % 100 < 14) {
+    return number + "th";
+  }
+  // if ends in 1, then st
+  if (number % 10 === 1) {
+    return number + "st";
+  }
+  // if ends in 2, then nd
+  if (number % 10 === 2) {
+    return number + "nd";
+  }
+  // if ends in 3, then rd
+  if (number % 10 === 3) {
+    return number + "rd";
+  }
+  // th otherwise
+  return number + "th";
 };
 
 module.exports = {
