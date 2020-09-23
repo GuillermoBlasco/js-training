09f38119ff908d3658c4a5eeda8277a697927a41
guillermo <gblasco@bloobirds.com>
2020-09-23 20:05:52 +0200

    fix on test 1-2

--- level-1/1-2-repeating-tasks/index.test.js
+++ level-1/1-2-repeating-tasks/index.test.js
@@ -1,5 +1,5 @@
 const scheduleTasks = require('./index').scheduleTasks;
 
-test('1 time', () => expect(scheduleTasks({ starting: '2020-01-01', repeat: 1, each: 2})).toBe(['2020-01-01']));
-test('0 times', () => expect(scheduleTasks({ starting: '2020-01-01', repeat: 0, each: 2})).toBe([]));
-test('3 times', () => expect(scheduleTasks({ starting: '2020-01-01', repeat: 3, each: 2})).toBe(['2020-01-01', '2020-01-03', '2020-01-07']));
+test('1 time', () => expect(scheduleTasks({ starting: '2020-01-01', repeat: 1, each: 2})).toStrictEqual(['2020-01-01']));
+test('0 times', () => expect(scheduleTasks({ starting: '2020-01-01', repeat: 0, each: 2})).toStrictEqual([]));
+test('3 times', () => expect(scheduleTasks({ starting: '2020-01-01', repeat: 3, each: 2})).toStrictEqual(['2020-01-01', '2020-01-03', '2020-01-07']));
