05d6ace34372b9e14be09bad27e8d9f9df428cd2
Jose Manuel Cano <jcano@bloobirds.com>
2020-09-26 12:17:11 +0200

    Checkbox Exercices

--- react-2/2-5-checkboxes/src/App.test.js
+++ react-2/2-5-checkboxes/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent } from '@testing-library/react';
+import CheckboxExercice from './CheckboxExercice';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Checkbox exists', () => {
+  const { getByText} = render(<CheckboxExercice />);
+  const inputElement = getByText(/Check Me!/i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('Checkbox checks', () => {
+  const { getByTestId } = render(<CheckboxExercice />);
+  expect(getByTestId('checkbox').checked).toBe(false);
+  fireEvent.click(getByTestId('checkbox'));
+  expect(getByTestId('checkbox').checked).toBe(true);
 });
--- react-2/2-5-checkboxes/src/CheckboxExercice.js
+++ react-2/2-5-checkboxes/src/CheckboxExercice.js
@@ -1,20 +1,22 @@
 import React, {useState} from 'react';
-
-const  Checkbox = () => {
+import './CheckboxExercice.css';
+const  CheckboxExercice = () => {
   const [checkValue, setCheckValue] = useState(false);
 
   const handleChange = () => {
     // your code here
   }
 
+  /*do not change the data-testid values (we use them for the auto evaluation)*/
   return (
     <div className="checkbox">
       <label>
         <input type="checkbox" data-testid="checkbox"/>
         Check Me!
       </label>
+      <div className="result"> The checkbox is: {checkValue ? 'Checked': 'Unchecked'}</div>
     </div>
   );
 }
 
-export default Checkbox;
+export default CheckboxExercice;
