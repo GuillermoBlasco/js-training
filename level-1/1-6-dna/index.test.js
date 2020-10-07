5831203934821fb6eaa1e8371baa3f0bcb4f7db6
guillermo <gblasco@bloobirds.com>
2020-10-07 20:41:30 +0200

    Fixed tests

--- level-1/1-6-dna/index.test.js
+++ level-1/1-6-dna/index.test.js
@@ -5,4 +5,4 @@ test('ok size 0', () => expect(dnaCheck('','')).toStrictEqual([]));
 test('ok size 5', () => expect(dnaCheck('AGTTC','TCAAG')).toStrictEqual([0,0,0,0,0]));
 test('bad size 5', () => expect(dnaCheck('AGTTA','TCAAG')).toStrictEqual([0,0,0,0,1]));
 test('bad size 5', () => expect(dnaCheck('AGTAA','TCAAG')).toStrictEqual([0,0,0,1,1]));
-test('bad size 7', () => expect(dnaCheck('AAAAAAA','AAAAAAA')).toStrictEqual([1,1,1,1,1,1]));
+test('bad size 7', () => expect(dnaCheck('AAAAAAA','AAAAAAA')).toStrictEqual([1,1,1,1,1,1,1]));
