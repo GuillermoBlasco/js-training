b4a3abb49cbaf51d9ea3c0c39cc0c602b07d4afe
Jose Manuel Cano <jcano@bloobirds.com>
2020-10-07 20:15:20 +0200

    Select component done

--- react-2/2-11-select/src/select/select.js
+++ react-2/2-11-select/src/select/select.js
@@ -1,12 +1,29 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styles  from './select.module.css';
-import info from './info.json';
 
 const Select = props => {
 	const {label, options, value} = props;
 	const [selectedValue, setSelectedValue] = useState(value);
 	const [renderedText, setRenderedText] = useState(label);
+	const [openDrop, setOpenDrop] = useState(false);
+	const refDrop = useRef(null);
+	useEffect(() => {
 
+		/**
+		 * Alert if clicked on outside of element
+		 */
+		function handleClickOutside(event) {
+			if (refDrop.current && !refDrop.current.contains(event.target)) {
+				setOpenDrop(false);
+			}
+		}
+		// Bind the event listener
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			// Unbind the event listener on clean up
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [refDrop]);
 	const handleClickOption = (opt) => {
 		if(opt.value === undefined){
 			setSelectedValue(undefined);
@@ -15,30 +32,32 @@ const Select = props => {
 			setSelectedValue(opt.value);
 			setRenderedText(opt.text);
 		}
+		setOpenDrop(false);
 	}
 
 	const isSelected = (value) => value === selectedValue;
 
 	return (
+		<>
 		<div className={styles.root}>
-			<div className={styles.label}>
+			<div className={styles.label} onClick={() => setOpenDrop(!openDrop)}>
 				{renderedText}
 			</div>
-			<div className={styles.dropdown}>
+			<div className={styles.dropdown} style={{visibility: !openDrop ? "hidden" : undefined }} ref={refDrop}>
 				<div className={styles.option}
-						 style={isSelected(undefined) ? {backgroundColor: "red"} : undefined}
 						 onClick={() => handleClickOption({text: "", value: undefined})}>
-					{"Nothing Selected"}
+					{" "}
 				</div>
 				{options && (options.map(opt => (
 					<div className={styles.option}
-							 style={isSelected(opt.value) ? {backgroundColor: "red"} : undefined}
+							 style={isSelected(opt.value) ? {backgroundColor: "#d4cfcc"} : undefined}
 							 onClick={() => handleClickOption(opt)}>
 						{opt.text}
 					</div>
 				)))}
 			</div>
 		</div>
+		</>
 	)
 };
 
